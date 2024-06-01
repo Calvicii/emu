979c4d60f7f2f5cd2240cf0045949661c4e20113
Thomas Lavoie <lavoiethomas17@gmail.com>
2024-05-31 22:08:07 -0400

    multiple chats

---
+++ app/_layout.jsx
@@ -0,0 +1,279 @@
+import 'react-native-gesture-handler';
+import { Drawer } from 'react-native-drawer-layout';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { MD3DarkTheme, PaperProvider, ActivityIndicator, IconButton, Button, Divider } from 'react-native-paper';
+import { Link, Stack, router } from 'expo-router';
+import { useFonts } from 'expo-font';
+
+const theme = {
+  ...MD3DarkTheme,
+  colors: {
+    ...MD3DarkTheme.colors,
+    primary: 'white',
+    background: '#333',
+    header: '#444',
+  },
+};
+
+export async function getChats() {
+  try {
+    const value = await AsyncStorage.getItem("chats");
+    return value !== null ? JSON.parse(value) : [];
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
+}
+
+export async function storeChats(chats) {
+  try {
+    await AsyncStorage.setItem("chats", JSON.stringify(chats));
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function storeChatMessages(chatId, messages) {
+  try {
+    let chats = await getChats();
+    console.log("Existing chats:", chats);
+    const chatIndex = chats.findIndex(chat => chat.id === parseInt(chatId));
+    console.log("Chat index:", chatIndex);
+    if (chatIndex !== -1) {
+      chats[chatIndex].messages = messages;
+      await storeChats(chats);
+    } else {
+      console.error(`Chat with id ${chatId} not found.`);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function deleteChat(id) {
+  try {
+    const chats = await getChats();
+    const updatedChats = chats.filter(chat => chat.id !== id);
+    await storeChats(updatedChats);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export default function RootLayout() {
+
+  const [currentChatId, setCurrentChatId] = useState();
+
+  const [open, setOpen] = useState(false);
+  const [chats, setChats] = useState([]);
+  
+  const [fontsLoaded] = useFonts({
+    'Outfit': require('../assets/fonts/Outfit-VariableFont.ttf'),
+    'Outfit-Black': require('../assets/fonts/Outfit-Black.ttf'),
+    'Outfit-Bold': require('../assets/fonts/Outfit-Bold.ttf'),
+    'Outfit-ExtraBold': require('../assets/fonts/Outfit-ExtraBold.ttf'),
+    'Outfit-ExtraLight': require('../assets/fonts/Outfit-ExtraLight.ttf'),
+    'Outfit-Light': require('../assets/fonts/Outfit-Light.ttf'),
+    'Outfit-Medium': require('../assets/fonts/Outfit-Medium.ttf'),
+    'Outfit-Regular': require('../assets/fonts/Outfit-Regular.ttf'),
+    'Outfit-SemiBold': require('../assets/fonts/Outfit-SemiBold.ttf'),
+    'Outfit-Thin': require('../assets/fonts/Outfit-Thin.ttf'),
+  });
+
+  useEffect(() => {
+    async function loadChats() {
+      const storedChats = await getChats();
+      setChats(storedChats);
+    }
+    loadChats();
+  }, []);
+
+  // Show a loading screen if the fonts have not loaded yet
+  if (!fontsLoaded) {
+    return (
+      <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
+        <ActivityIndicator style={{ flex: 1 }} animating={true} size="large" color="white" />
+      </View>
+    );
+  }
+
+  async function newChat() {
+    let chats = await getChats();
+    let newChatId;
+    if (chats.length === 0) {
+      newChatId = 0;
+      chats.push({ id: newChatId, messages: [] });
+    } else {
+      newChatId = chats[chats.length - 1].id + 1;
+      chats.push({ id: newChatId, messages: [] });
+    }
+    await storeChats(chats);
+    setChats(chats);
+    return newChatId;
+  }
+
+  async function handleDeleteChat(id) {
+    await deleteChat(id);
+    const updatedChats = await getChats();
+    setChats(updatedChats);
+  }
+
+  return (
+    <PaperProvider theme={theme}>
+      <Drawer
+        drawerType="slide"
+        drawerStyle={{
+          backgroundColor: theme.colors.background,
+          width: 250,
+        }}
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        renderDrawerContent={() => {
+          return (
+            <View style={styles.drawer}>
+              <View style={styles.chats}>
+                <Button
+                  mode="outlined"
+                  icon="plus"
+                  textColor={theme.colors.primary}
+                  onPress={async () => {
+                    const newChatId = await newChat();
+                  }}
+                >
+                  New Chat
+                </Button>
+                <Divider style={styles.divider} />
+                <FlatList
+                  style={styles.chatList}
+                  data={chats}
+                  keyExtractor={(item) => item.id.toString()}
+                  renderItem={({ item }) => (
+                    <View style={styles.chatItem}>
+                      <Button
+                        style={{ flex: 1 }}
+                        mode="outlined"
+                        textColor={theme.colors.primary}
+                        onPress={() => {
+                          setOpen(false);
+                          setCurrentChatId(item.id);
+                          router.navigate({
+                            pathname: "",
+                            params: {
+                              chatId: item.id,
+                            }
+                          })
+                        }}
+                      >
+                        Chat {item.id}
+                      </Button>
+                      <IconButton
+                        icon="delete"
+                        iconColor={theme.colors.primary}
+                        onPress={() => {
+                          handleDeleteChat(item.id);
+                          setCurrentChatId(undefined);
+                          router.navigate({
+                            pathname: "",
+                            params: {
+                              chatId: undefined,
+                            }
+                          })
+                        }}
+                      />
+                    </View>
+                  )}
+                />
+              </View>
+              <Link href="settings" asChild>
+                <Button
+                  style={styles.navSettings}
+                  mode="contained-tonal"
+                  icon="cog"
+                  onPress={() => setOpen(false)}
+                >
+                  Settings
+                </Button>
+              </Link>
+            </View>
+          );
+        }}
+      >
+        <Stack>
+          <Stack.Screen
+            name="index"
+            initialParams={{
+              currentChatId: 1,
+            }}
+            options={{
+              title: "Chat " + currentChatId,
+              statusBarColor: theme.colors.background,
+              headerStyle: {
+                backgroundColor: theme.colors.header,
+              },
+              headerTitleStyle: {
+                fontFamily: "Outfit-Bold",
+                color: theme.colors.primary,
+              },
+              headerLeft: () => (
+                <IconButton
+                  iconColor="white"
+                  icon="menu"
+                  onPress={() => setOpen(true)}
+                />
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="settings"
+            options={{
+              title: "Settings",
+              statusBarColor: theme.colors.background,
+              headerStyle: {
+                backgroundColor: theme.colors.header,
+              },
+              headerTitleStyle: {
+                fontFamily: "Outfit-Bold",
+                color: theme.colors.primary,
+              },
+              headerLeft: () => (
+                <IconButton
+                  iconColor="white"
+                  icon="arrow-left"
+                  onPress={() => router.back()}
+                />
+              ),
+            }}
+          />
+        </Stack>
+      </Drawer>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  drawer: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 5,
+  },
+  chats: {
+    flex: 1,
+  },
+  chatItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  navSettings: {
+    flex: 0,
+  },
+  divider: {
+    marginVertical: 10,
+    height: 2,
+  },
+  chatList: {
+    marginBottom: 10,
+  }
+});
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
-import { useTheme, TextInput, Text, IconButton, Button, Surface, Portal, Modal, ActivityIndicator } from 'react-native-paper';
+import { useTheme, TextInput, Text, IconButton, Button, Surface, Portal, Modal } from 'react-native-paper';
+import { useLocalSearchParams } from "expo-router";
 import { getSetting } from './settings';
+import { getChats, storeChatMessages } from './_layout';
 
 export default function Index() {
+
+  // Get the selected chat's ID
+  const { chatId } = useLocalSearchParams();
 
   // For global theming
   const theme = useTheme();
 
-  // Ollama server's ip
+  // Ollama server's IP
   const [ip, setIp] = useState("");
 
   // For the modal's functions
@@ -28,6 +33,32 @@ export default function Index() {
   // The chat (includes user's chat and the model's chat)
   const [chat, setChat] = useState([]);
 
+  // Refresh the model list and grab the chat from storage
+  useEffect(() => {
+    console.log("refresh");
+  
+    if (chatId !== undefined) {
+      async function retrieveChat() {
+        const chats = await getChats();
+        const chatIndex = chats.findIndex(chat => chat.id === parseInt(chatId));
+        if (chatIndex !== -1) {
+          const messages = chats[chatIndex].messages;
+          console.log(messages);
+          setChat(messages);
+        }
+      }      
+      retrieveChat();
+
+      getModels();
+
+      async function fetchSettings() {
+        const savedIp = await getSetting("ip");
+        setIp(savedIp);
+      }
+      fetchSettings();
+    }
+  }, [chatId, visible]);  
+
   // Boolean that indicates if the model is working on a response
   const [loading, setLoading] = useState(false);
 
@@ -37,17 +68,8 @@ export default function Index() {
   // Reference the chat's FlatList
   const chatListRef = useRef();
 
-  // Grab models and settings on modal show/hide
-  useEffect(() => {
-    getModels();
-    console.log("refresh");
-
-    async function fetchSettings() {
-      const savedIp = await getSetting("ip");
-      setIp(savedIp);
-    }
-    fetchSettings();
-  }, [visible]);
+  // Error messages
+  const connErr = "Connection failed. Make sure the IP address is correct.";
 
   // Grab the models available on the machine
   async function getModels() {
@@ -65,7 +87,7 @@ export default function Index() {
       setErrorMessage("");
 
     } catch (error) {
-      setErrorMessage("The connection failed. Make sure the IP address is correct.");
+      setErrorMessage(connErr);
       setModels("");
       setSelectedModel("");
     }
@@ -73,26 +95,23 @@ export default function Index() {
 
   async function sendPrompt() {
     if (prompt !== "" && selectedModel !== "") {
-
-      // Reset the TextInput
-      setPrompt("");
-
-      // Scroll to the message that was sent
-      chatListRef.current.scrollToEnd();
-
-      // Format the user's prompt
-      let userMessage = {
-        role: "user",
-        content: prompt,
-      };
-
-      // Add it to the chat
-      setChat([...chat, userMessage]);
-
-      // Set loading to disable inputs
-      setLoading(true);
-
       try {
+        // Reset the TextInput
+        setPrompt("");
+  
+        // Set loading to disable inputs
+        setLoading(true);
+  
+        // Scroll to the message that was sent
+        chatListRef.current.scrollToEnd();
+  
+        // Create the user's message object
+        const userMessage = {
+          role: "user",
+          content: prompt,
+        };
+  
+        // Send the request to the server
         const response = await fetch(`http://${ip}/api/chat`, {
           method: "POST",
           headers: {
@@ -104,129 +123,148 @@ export default function Index() {
             stream: false,
           }),
         });
-
-        // Receive the data
+  
+        // Check if the response is successful
+        if (!response.ok) {
+          throw new Error("Failed to send message.");
+        }
+  
+        // Receive the data from the response
         const data = await response.json();
-
-        // Add response to the chat
-        setChat([...chat, userMessage, data.message]);
+  
+        // Update the chat state with the response and user's message
+        setChat(prevChat => [...prevChat, userMessage, data.message]);
+  
+        // Store the updated chat state
+        storeChatMessages(chatId, [...chat, userMessage, data.message]);
+  
+        // Clear any error message
         setErrorMessage("");
-
       } catch (error) {
-        setErrorMessage("The connection failed. Make sure the IP address is correct.");
+        console.error(error);
+        setErrorMessage(connErr);
       } finally {
-
         // Scroll to response
         chatListRef.current.scrollToEnd();
-
+  
         // Set loading to enable inputs
         setLoading(false);
-      }  
+      }
     }
   }
 
-  return (
-    <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
-      <Portal>
-        <Modal visible={visible} onDismiss={hideModal}>
-          {models.length > 0 ? (
-            <FlatList
-              style={styles.modelList}
-              data={models}
-              keyExtractor={(item) => item.name}
-              renderItem={({ item }) => (
-                <Button
-                  labelStyle={styles.modelListLabel}
-                  onPress={() => {
-                    setSelectedModel(item.name);
-                    hideModal();
-                  }}
-                >
-                  {item.name}
+  if (chatId === undefined) {
+    return (
+      <View style={styles.noChatView}>
+        <Text style={styles.noChatLabel}>Select or create a chat</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
+        <Portal>
+          <Modal visible={visible} onDismiss={hideModal}>
+            {models.length > 0 ? (
+              <FlatList
+                style={styles.modelList}
+                data={models}
+                keyExtractor={(item) => item.name}
+                renderItem={({ item }) => (
+                  <Button
+                    labelStyle={styles.modelListLabel}
+                    onPress={() => {
+                      setSelectedModel(item.name);
+                      hideModal();
+                    }}
+                  >
+                    {item.name}
+                  </Button>
+                )}
+              />
+              ) : (
+                <View style={styles.modelList}>
+                  <Text style={styles.modelListLabel}>No models available</Text>
+                </View>
+              )
+            }
+          </Modal>
+        </Portal>
+  
+        <View style={styles.window}>
+  
+          <View style={styles.options}>
+  
+          {selectedModel === "" ? (
+                <Button style={styles.selectedMode} onPress={showModal} labelStyle={styles.selectedModeLabel}>
+                  Select a model
                 </Button>
-              )}
-            />
             ) : (
-              <View style={styles.modelList}>
-                <Text style={styles.modelListLabel}>No models available</Text>
-              </View>
-            )
-          }
-        </Modal>
-      </Portal>
-
-      <View style={styles.window}>
-
-        <View style={styles.options}>
-
-        {selectedModel === "" ? (
               <Button style={styles.selectedMode} onPress={showModal} labelStyle={styles.selectedModeLabel}>
-                Select a model
+                {selectedModel}
               </Button>
-          ) : (
-            <Button style={styles.selectedMode} onPress={showModal} labelStyle={styles.selectedModeLabel}>
-              {selectedModel}
-            </Button>
-          )
-        }
-
-        </View>
-
-        <View style={styles.chat}>
-
-          {errorMessage !== "" ? (
-              <Text style={styles.errorMessage}>{errorMessage}</Text>
-            ) : (
-              <></>
             )
           }
-
-          <FlatList
-            ref={chatListRef}
-            data={chat}
-            renderItem={({ item }) => {
-              if (item.role === "assistant") {
-                return (
-                  <Surface style={styles.modelBubble} mode="flat">
-                    <Text style={styles.bubbleLabel}>{item.content}</Text>
-                  </Surface>
-                );
-              }
-              if (item.role === "user") {
-                return (
-                  <Surface style={styles.userBubble} mode="elevated">
-                    <Text style={styles.bubbleLabel}>{item.content}</Text>
-                  </Surface>
-                );
-              }
-            }}
+  
+          </View>
+  
+          <View style={styles.chat}>
+  
+            {errorMessage !== "" ? (
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
+              ) : (
+                <></>
+              )
+            }
+  
+            <FlatList
+              ref={chatListRef}
+              data={chat}
+              renderItem={({ item }) => {
+                if (item !== undefined) {
+                  if (item.role === "assistant") {
+                    return (
+                      <Surface style={styles.modelBubble} mode="flat">
+                        <Text style={styles.bubbleLabel}>{item.content}</Text>
+                      </Surface>
+                    );
+                  }
+                  if (item.role === "user") {
+                    return (
+                      <Surface style={styles.userBubble} mode="elevated">
+                        <Text style={styles.bubbleLabel}>{item.content}</Text>
+                      </Surface>
+                    );
+                  }
+                }
+              }}
+            />
+          </View>
+  
+        </View>
+  
+        <View style={styles.controls}>
+          <TextInput
+            style={styles.input}
+            contentStyle={styles.inputValue}
+            mode="outlined"
+            placeholder={"Message " + selectedModel}
+            value={prompt}
+            disabled={loading}
+            onChangeText={(val) => setPrompt(val)}
+          />
+          <IconButton
+            style={styles.sendButton}
+            mode="contained"
+            icon="arrow-up"
+            disabled={loading}
+            loading={loading}
+            onPress={sendPrompt}
           />
         </View>
-
+        
       </View>
-
-      <View style={styles.controls}>
-        <TextInput
-          style={styles.input}
-          contentStyle={styles.inputValue}
-          mode="outlined"
-          placeholder={"Message " + selectedModel}
-          value={prompt}
-          disabled={loading}
-          onChangeText={(val) => setPrompt(val)}
-        />
-        <IconButton
-          style={styles.sendButton}
-          mode="contained"
-          icon="arrow-up"
-          disabled={loading}
-          loading={loading}
-          onPress={sendPrompt}
-        />
-      </View>
-      
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -298,5 +336,15 @@ const styles = StyleSheet.create({
     fontSize: 14,
     textAlign: "center",
     color: "red"
+  },
+  noChatView: {
+    flex: 1,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  noChatLabel: {
+    fontSize: 30,
+    fontFamily: "Outfit-Regular"
   }
 });
